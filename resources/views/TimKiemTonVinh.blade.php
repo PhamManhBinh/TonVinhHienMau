2b03a6570bcddd11f5bfe9497bfd37f5fb3a085a
PhamManhBinh <phammanhbinh10a3@gmail.com>
2021-12-06 13:55:29 +0700

    fix sap xep

--- resources/views/TimKiemTonVinh.blade.php
+++ resources/views/TimKiemTonVinh.blade.php
@@ -229,39 +229,67 @@ function sapXep() {
     var selectedValue = selectBox.options[selectBox.selectedIndex].value;
     sortTable("tableData",selectedValue);
 }
-function sortTable(idTable,rowIndex) {
-  var table, rows, switching, i, x, y, shouldSwitch;
-  table = document.getElementById(idTable);
-  switching = true;
-  /*Make a loop that will continue until
-  no switching has been done:*/
-  while (switching) {
-    //start by saying: no switching is done:
-    switching = false;
-    rows = table.rows;
-    /*Loop through all table rows (except the
-    first, which contains table headers):*/
-    for (i = 1; i < (rows.length - 1); i++) {
-      //start by saying there should be no switching:
-      shouldSwitch = false;
-      /*Get the two elements you want to compare,
-      one from current row and one from the next:*/
-      x = rows[i].getElementsByTagName("TD")[rowIndex];
-      y = rows[i + 1].getElementsByTagName("TD")[rowIndex];
-      if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
-        //if so, mark as a switch and break the loop:
-        shouldSwitch = true;
-        break;
-        
-      }
+
+function sortTable(table_id, sortColumn){
+    var tableData = document.getElementById(table_id).getElementsByTagName('tbody').item(0);
+    var rowData = tableData.getElementsByTagName('tr');            
+    for(var i = 0; i < rowData.length - 1; i++){
+        for(var j = 0; j < rowData.length - (i + 1); j++){
+            //sort by name
+            if(sortColumn == 0){
+              x = rowData[j].getElementsByTagName("TD")[sortColumn];
+              y = rowData[j+1].getElementsByTagName("TD")[sortColumn];
+              if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
+                tableData.insertBefore(rowData.item(j+1),rowData.item(j));
+              }
+            }else
+            //sort by number
+            if(Number(rowData.item(j).getElementsByTagName('td').item(sortColumn).innerHTML.replace(/[^0-9\.]+/g, "")) < Number(rowData.item(j+1).getElementsByTagName('td').item(sortColumn).innerHTML.replace(/[^0-9\.]+/g, ""))){
+                tableData.insertBefore(rowData.item(j+1),rowData.item(j));
+            }
+        }
     }
-    if (shouldSwitch) {
-      /*If a switch has been marked, make the switch
-      and mark that a switch has been done:*/
-      rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
-      switching = true;
-    }
-  }
 }
+  // var table, rows, switching, i, x, y, shouldSwitch;
+  // table = document.getElementById(idTable);
+  // switching = true;
+  // /*Make a loop that will continue until
+  // no switching has been done:*/
+  // while (switching) {
+    
+  // //   //start by saying: no switching is done:
+  // //   switching = false;
+  // //   rows = table.rows;
+  // //   /*Loop through all table rows (except the
+  // //   first, which contains table headers):*/
+  // //   for (i = 1; i < (rows.length - 1); i++) {
+  // //     //start by saying there should be no switching:
+  // //     shouldSwitch = false;
+  // //     /*Get the two elements you want to compare,
+  // //     one from current row and one from the next:*/
+  // //     x = rows[i].getElementsByTagName("TD")[rowIndex];
+  // //     y = rows[i + 1].getElementsByTagName("TD")[rowIndex];
+  // //     if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
+  // //       if(rowIndex == 5){
+  // //         if(parseInt(x.innerHTML.toLowerCase())>parseInt(y.innerHTML.toLowerCase())){
+  // //           shouldSwitch = true;
+  // //           break;
+  // //         }
+  // //       }else{
+  // //         //if so, mark as a switch and break the loop:
+  // //         shouldSwitch = true;
+  // //         break;
+  // //       }
+        
+  // //     }
+  // //   }
+  // //   if (shouldSwitch) {
+  // //     /*If a switch has been marked, make the switch
+  // //     and mark that a switch has been done:*/
+  // //     rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+  // //     switching = true;
+  // //   }
+  // // }
+
 </script>
 @endsection
